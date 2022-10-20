4eb152fff817043315ca7e6b925fa45230ef60de
Dendy07 <dendyscik@gmal.com>
2022-10-20 13:50:51 +0700

    menambahkan file /c/Users/Aspire-ES14/LatihanPraktikum/Latihanduaobjek/src/main/java/com/mycompany/latihanduaobjek/Latihanduaobjek.java

---
+++ Latihan1/src/main/java/com/mycompany/latihan1/Latihan1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.latihan1;
+
+/**
+ *
+ * @author Aspire-ES14
+ */
+public class Latihan1 {
+
+    public static void main(String[] args) {
+        System.out.println("Class Person");
+        System.out.println(" ");
+        /* memanggil atribut */
+        System.out.println("Nama: Desatra");
+        System.out.println("Jenis Kelamin : Laki-laki");
+        System.out.println("Umur : 19");
+    }
+}
